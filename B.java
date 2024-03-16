0b2add1f5ab7367f92c27a2be9d3902f103d415c
Vamsi Kumar <Vamsikumar0006@gmail.com>
2024-03-16 18:33:05 +0530

    Commiting B.java File By Vamsi

---
+++ B.java
@@ -0,0 +1,27 @@
+package pack2;
+
+import pack1.A;
+
+public class B extends A
+{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		A aobj = new A();
+		obj.m1();
+		*/
+		
+		B bobj = new B();   //Through inheritance
+		bobj.m1();
+		
+		B newobj = new B();  // 
+		newobj.add();
+		
+		
+		
+
+	}
+
+}
